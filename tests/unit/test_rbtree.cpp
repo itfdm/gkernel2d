ce787c0b578117f3f43f47bf4ac1a32479b0addd
spitefulowl <spitefulowll@gmail.com>
2022-10-30 18:58:27 +0300

    Add more tests

--- tests/unit/test_rbtree.cpp
+++ tests/unit/test_rbtree.cpp
@@ -8,6 +8,8 @@ void test_insert()
     int_tree tree;
     tree.insert({5, 3, 9, 6, 1});
     REQUIRE(tree.size() == 5);
+    tree.insert(5);
+    REQUIRE(tree.size() == 5);
     REQUIRE(tree.min() == 1);
     REQUIRE(tree.max() == 9);
 }
@@ -29,6 +31,20 @@ void test_prev_next() {
     REQUIRE(tree.size() == 5);
 }
 
+void test_erase() {
+    int_tree tree;
+    tree.insert({55, 2342, 123, 543, -123, 190, 500});
+    REQUIRE(tree.size() == 7);
+    tree.erase(55);
+    REQUIRE(tree.size() == 6);
+    tree.erase(55);
+    REQUIRE(tree.size() == 6);
+    tree.erase(-123);
+    tree.erase(500);
+    tree.erase(2342);
+    REQUIRE(tree.size() == 3);
+}
+
 TEST_CASE("Test item insertion")
 {
     test_insert();
@@ -36,4 +52,8 @@ TEST_CASE("Test item insertion")
 
 TEST_CASE("Test prev/next") {
     test_prev_next();
+}
+
+TEST_CASE("Test erase") {
+    test_erase();
 }
