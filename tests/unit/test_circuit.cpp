ed5cf66014d0fbbcfc301bf7db2a1b84d5a8adda
spitefulowl <54745281+spitefulowl@users.noreply.github.com>
2022-11-05 14:29:49 +0300

    Add RBTree implementation (std::set wrapper) + tests (#1)

---
+++ tests/unit/test_circuit.cpp
@@ -0,0 +1,15 @@
+#include "gkernel/circuit.h"
+#include "test.hpp"
+
+void test_point_insert()
+{
+    gkernel::Circuit circuit(1);
+    circuit.push({42, 15});
+    REQUIRE(circuit[0].x == 42);
+    REQUIRE(circuit[0].y == 15);
+}
+
+TEST_CASE("Test one point insertion into circuit")
+{
+    test_point_insert();
+}
