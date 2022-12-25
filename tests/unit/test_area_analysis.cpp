85bf5d7287a012f547491bae2f985d514b7f56c2
IlyaShein <petrol.iva@yandex.ru>
2022-12-25 23:34:12 +0300

    Add test

--- tests/unit/test_area_analysis.cpp
+++ tests/unit/test_area_analysis.cpp
@@ -56,53 +56,32 @@ void TestAreasSimple() {
 }
 
 void TestAreasDefault() {
-    // std::vector<Segment> segments = {
-    //     {{1, 3}, {3, 5}},
-    //     {{3, 5}, {6, 8}},
-    //     {{6, 8}, {10, 8}},
-    //     {{10, 8}, {11.5, 5.5}},
-    //     {{11.5, 5.5}, {13, 3}},
-    //     {{13, 3}, {9, 3}},
-    //     {{9, 3}, {5, 3}},
-    //     {{5, 3}, {1, 3}}, //
-    //     {{1, 7}, {6, 12}},
-    //     {{6, 12}, {10, 8}},
-    //     {{10, 8}, {12, 6}},
-    //     {{12, 6}, {11.5, 5.5}},
-    //     {{11.5, 5.5}, {9, 3}},
-    //     {{9, 3}, {7, 1}},
-    //     {{7, 1}, {5, 3}},
-    //     {{5, 3}, {3, 5}},
-    //     {{3, 5}, {1, 7}}
-    // };
-
     std::vector<Segment> segments = {
-        {{2, 1}, {2, 4}},
-        {{2, 4}, {2, 5}},
-        {{2, 5}, {2, 7}},
-        {{2, 7}, {4, 7}},
-        {{4, 7}, {6, 7}},
-        {{6, 7}, {8, 7}},
-        {{8, 7}, {8, 5}},
-        {{8, 5}, {8, 4}},
-        {{8, 4}, {8, 1}},
-        {{8, 1}, {2, 1}},  //
-        {{1, 4}, {2, 5}},
-        {{2, 5}, {4, 7}},
-        {{4, 7}, {5, 8}},
-        {{5, 8}, {6, 7}},
-        {{6, 7}, {8, 5}},
-        {{8, 5}, {9, 4}},
-        {{9, 4}, {8, 4}},
-        {{8, 4}, {2, 4}},
-        {{2, 4}, {1, 4}}
+        {{1, 3}, {3, 5}},
+        {{3, 5}, {6, 8}},
+        {{6, 8}, {10, 8}},
+        {{10, 8}, {11.5, 5.5}},
+        {{11.5, 5.5}, {13, 3}},
+        {{13, 3}, {9, 3}},
+        {{9, 3}, {5, 3}},
+        {{5, 3}, {1, 3}}, //
+        {{1, 7}, {6, 12}},
+        {{6, 12}, {10, 8}},
+        {{10, 8}, {12, 6}},
+        {{12, 6}, {11.5, 5.5}},
+        {{11.5, 5.5}, {9, 3}},
+        {{9, 3}, {7, 1}},
+        {{7, 1}, {5, 3}},
+        {{5, 3}, {3, 5}},
+        {{3, 5}, {1, 7}}
     };
+
     SegmentsSet layer(segments);
     layer.set_labels_types({ test_labels::circuits_layer_id });
-    for (std::size_t idx = 0; idx < 10; ++idx) {
+    for (std::size_t idx = 0; idx < 8; ++idx) {
         layer.set_label_value(test_labels::circuits_layer_id, layer[idx], 0);
     }
-    for (std::size_t idx = 10; idx < layer.size(); ++idx) {
+    for (std::size_t idx = 8; idx < layer.size(); ++idx) {
         layer.set_label_value(test_labels::circuits_layer_id, layer[idx], 1);
     }
 
@@ -123,6 +102,51 @@ void TestAreasDefault() {
             std::cout << "bottom neighbour not found" << std::endl;
         }
         std::cout << std::endl;
+    }
+}
+
+void TestAreasVert() {
+    std::vector<Segment> input_segments = {
+        {{2, 1}, {2, 4}},
+        {{2, 4}, {2, 5}},
+        {{2, 5}, {2, 7}},
+        {{2, 7}, {4, 7}},
+        {{4, 7}, {6, 7}},
+        {{6, 7}, {8, 7}},
+        {{8, 7}, {8, 5}},
+        {{8, 5}, {8, 4}}, 
+        {{8, 4}, {8, 1}},
+        {{8, 1}, {2, 1}},  //
+        {{1, 4}, {2, 5}},
+        {{2, 5}, {4, 7}},
+        {{4, 7}, {5, 8}},
+        {{5, 8}, {6, 7}},
+        {{6, 7}, {8, 5}},
+        {{8, 5}, {9, 4}},
+        {{9, 4}, {8, 4}},
+        {{8, 4}, {2, 4}},
+        {{2, 4}, {1, 4}}
+    };
+
+    SegmentsSet expected = input_segments;
+
+    expected.set_labels_types({0, 1, 2});
+    expected.set_label_values(0, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1});
+    expected.set_label_values(1, {17, 11, 3, -1, 12, -1, -1, 15, 16, 17, -1, 3, -1, -1, 5, -1, 15, 11, 10});
+    expected.set_label_values(2, {9, 17, 11, 11, 17, 14, 16, 16, -1, -1, 18, 17, 4, 4, 17, 16, -1, 9, -1});
+
+    SegmentsSet test_layer = input_segments;
+    test_layer.set_labels_types({0, 1, 2});
+    test_layer.set_label_values(0, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1});
+
+    auto actual = AreaAnalysis::findSegmentsNeighbours(test_layer);
+
+    REQUIRE_EQ(actual.size(), expected.size());
+
+    for (std::size_t idx = 0; idx < expected.size(); ++idx) {
+        REQUIRE_EQ(actual.get_label_value(0, actual[idx]), expected.get_label_value(0, expected[idx]));
+        REQUIRE_EQ(actual.get_label_value(1, actual[idx]), expected.get_label_value(1, expected[idx]));
+        REQUIRE_EQ(actual.get_label_value(2, actual[idx]), expected.get_label_value(2, expected[idx]));
     }
 }
 
@@ -319,7 +343,7 @@ void TestAreasSecond() {
 }
 
 // DECLARE_TEST(TestAreasSimple);
-DECLARE_TEST(TestAreasDefault);
+DECLARE_TEST(TestAreasVert);
 DECLARE_TEST(TestAreasFirstPhase);
 DECLARE_TEST(TestAreasSecondPhase);
 DECLARE_TEST(TestAreasFirst);
