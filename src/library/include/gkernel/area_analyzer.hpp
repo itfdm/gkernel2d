997990556d353a131b9cf3ac2643a43e12ecb514
spitefulowl <spitefulowll@gmail.com>
2023-01-15 15:32:07 +0300

    Fix labels transfer

--- src/library/include/gkernel/area_analyzer.hpp
+++ src/library/include/gkernel/area_analyzer.hpp
@@ -23,15 +23,27 @@ public:
     static SegmentsLayer markAreas(const SegmentsLayer& layer);
 
     template<typename Callable>
-    static SegmentsLayer filterSegmentsByLabels(const SegmentsLayer& layer, Callable callable) {
+    static SegmentsLayer markAreasAndFilter(const SegmentsLayer& layer, Callable callable) {
         auto areas = findAreas(layer);
         std::vector<Segment> result;
+        std::vector<std::size_t> ids;
         for (std::size_t idx = 0; idx < areas.size(); ++idx) {
             if (callable(areas, layer[idx])) {
                 result.emplace_back(areas[idx]);
+                ids.emplace_back(idx);
             }
         }
-        return result;
+        if (result.empty()) {
+            return SegmentsLayer();
+        }
+        SegmentsSet layer_result(result);
+        layer_result.set_labels_types(layer.get_label_types());
+        for (std::size_t idx = 0; idx < ids.size(); ++idx) {
+            for (auto label : layer.get_label_types()) {
+                layer_result.set_label_value(label, layer_result[idx], layer.get_label_value(label, layer[ids[idx]]));
+            }
+        }
+        return layer_result;
     }
 };
 
